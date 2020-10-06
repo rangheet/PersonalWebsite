967e1dc962d8dad945ba55369b54384ae6708c5d
rangheet <heetdave@outlook.com>
2020-10-05 17:07:27 -0700

    Changed intro message

--- src/component/personal-info/sagas.js
+++ src/component/personal-info/sagas.js
@@ -17,8 +17,8 @@ function* getPersonalInfo()
         if(personalInfo==="Error!")
         {
             personalInfo = {
-                bio: "Hi! My name is Heet Dave and I am pursuing MS in Computer Science at Arizona State University. I'm currently seeking full-time opportunities starting in May 2021."
-                + " This summer, I was working as a Software Engineering Intern at Apollo Education group in Data Engineering Team."
+                bio: "Hi! My name is Heet Dave and I am pursuing MS in Computer Science at Arizona State University. Currently, I'm working as a Software Engineering Intern at Intel Corporation. I am seeking full-time opportunities starting in May 2021."
+                + " This summer, I worked as a Software Engineering Intern at Apollo Education group in Data Engineering Team."
                 + " I worked on creating data pipelines using Python for data coming from BlackBoard which involved the use of AWS Athena, Glue, DynamoDB, S3 and microservices."
                 + " Apart from this I've also worked as a Front End Engineer and Full Stack Engineer which involved a little bit of DevOps too."
                 + " I consider myself fortunate that I've got to work in different domains in such a short time. This has allowed me to explore different areas of Software Development and beyond."
